8945bab88a7bc48bc1b6628ac758beccad79e90a
pschaible <pschaible96@gmail.com>
2015-12-15 18:18:27 +0100

    first commit

---
+++ telebot_package/telebot.py
@@ -0,0 +1,25 @@
+
+import telebot
+
+from time import sleep
+from math import *
+from random import *
+
+bot = telebot.Bot(token='166712353:AAGQxJyriJEIvi2l4oSqMLcv1Wdhhpc8gFk')
+
+max_ids = {}
+
+while True:
+    updates = bot.getUpdates()
+    for  u in updates:
+        message = u.message
+        text = message['text']
+        message_id = message['message_id']
+        chat_id = message['chat']['id']
+        if chat_id not in max_ids or message_id > max_ids[chat_id]:
+            max_ids[chat_id] = message_id
+            try:
+                bot.sendMessage(chat_id=chat_id, text=str(eval(text)))
+            except:
+                pass
+    sleep(2)
