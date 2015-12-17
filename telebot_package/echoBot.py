7f742abee28d790d9e553a3f6e30b4afe4cfd4a0
pschaible <pschaible96@gmail.com>
2015-12-17 14:25:08 +0100

    integrated with rundom functions

--- telebot_package/echoBot.py
+++ telebot_package/echoBot.py
@@ -3,41 +3,63 @@ import telebot
 from telebot import types
 
 
+def rundomIntList(begin, end, number):
+    intList = []
+    while number > 0:
+        newNumber = random.randint(begin, end)
+        if newNumber in intList:
+            continue
+        else:
+            intList.append(newNumber)
+            number = number - 1
+    return intList
+
 dict = {
-    "Жидомор"               :   "Скупец, скряга",
-    "Иерусалимская слеза"   :   "Водка",
-    "Канапе"                :   "Небольшой диван с приподнятым изголовьем",
-    "Комиссия"              :   "Поручение; возня, хлопоты, заботы",
-    "Косушка"               :   "Полбутылки водки",
-    "Баргузин"              :   "Северо-восточный ветер на Байкале",}
+    "Жидомор": "Скупец, скряга",
+    "Иерусалимская слеза": "Водка",
+    "Канапе": "Небольшой диван с приподнятым изголовьем",
+    "Комиссия": "Поручение; возня, хлопоты, заботы",
+    "Косушка": "Полбутылки водки",
+    "Баргузин": "Северо-восточный ветер на Байкале",}
 
-
-words = ["Жидомор", "Иерусалимская слеза","Канапе"]
+words = ["Жидомор", "Иерусалимская слеза", "Канапе"]
 meanings = ["Скупец, скряга", "Водка", "Небольшой диван с приподнятым изголовьем"]
 selectedWord = "Жидомор"
 
 
-def correctInDict(aDic, aKey, aValue) :
-    if aKey in aDic :
+def correctInDict(aDic, aKey, aValue):
+    if aKey in aDic:
         return aDic.get(aKey) == aValue
     else:
         return False
 
+
 API_TOKEN = '166712353:AAGQxJyriJEIvi2l4oSqMLcv1Wdhhpc8gFk'
 bot = telebot.TeleBot(API_TOKEN)
+
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
 
+    global selectedWord, wrongWord1, wrongWord2
+    global selectedMeaning, wrongMeaning1, wrongMeaning2
 
-    global selectedWord
-    selectedWord= words[random.randint(0,len(words)-1)]
+    selectedWordIndexes = rundomIntList(0, len(words)-1, 3)
+
+    selectedWord = words[selectedWordIndexes[0]]
+    wrongWord1 =  words[selectedWordIndexes[1]]
+    wrongWord2 = words[selectedWordIndexes[2]]
+
+    selectedMeaning = meanings[selectedWordIndexes[0]]
+    wrongMeaning1 =  meanings[selectedWordIndexes[1]]
+    wrongMeaning2 = meanings[selectedWordIndexes[2]]
 
     markup = types.ReplyKeyboardMarkup()
-    markup.row(dir(selectedWord))
-    markup.row(meanings[random.randint(0,len(meanings)-1)])
-    markup.row(meanings[random.randint(0,len(meanings)-1)])
+
+    markup.row(selectedMeaning)
+    markup.row(wrongMeaning1)
+    markup.row(wrongMeaning2)
 
     bot.send_message(message.chat.id, "Здорово, товарищ! Выбери правильное значение для слова: "
                      + selectedWord, reply_markup=markup)
@@ -46,7 +68,6 @@ def send_welcome(message):
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def guess_message(message):
-
     if correctInDict(dict, selectedWord, message.text):
         bot.send_message(message.chat.id, "УРА .. ты молодец")
     else:
